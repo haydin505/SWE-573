25c3d6611389f77da87045376877c109f84c29ff
huseyinaydin505 <huseyin.aydin@migrosonline.com>
2022-11-08 21:43:48 +0300

    Fix jdbc url and index.html application name

--- infrasave/infrasave-react-client/src/Home.tsx
+++ infrasave/infrasave-react-client/src/Home.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import {Card, Layout} from "antd";
+import {Anchor, Card, Image, Layout} from "antd";
 import "./Home.css";
 
 const Home: React.FC = () => {
+	const { Link } = Anchor;
 	return <div className="site-card-border-less-wrapper">
 		<h1>Home</h1>
 		<Layout style={{
 			padding: '50px',
 			background: '#ececec'
 		}}>
-			<Card title={"1"} className="content">
-				<p>1</p>
-				<p>1</p>
-				<p>1</p>
-				<p>1</p>
+			<Card title={<Image title={"hey"} width={50} src="https://img.icons8.com/color/512/twitter.png"/>} className="content">
+				<h2>Analysis: Nagging U.S. Treasury liquidity problems raise Fed balance sheet predicament</h2>
+				<Anchor>
+					<Link target="_blank" href="https://twitter.com/Reuters/status/1590027914367713280" title="Link" />
+				</Anchor>
+				<Image width={450} src="https://pbs.twimg.com/media/FhDp9jIXEAIllKD?format=jpg&name=medium"/>
 			</Card>
 			<Card title={"2"} className="content">
 				<p>2</p>
