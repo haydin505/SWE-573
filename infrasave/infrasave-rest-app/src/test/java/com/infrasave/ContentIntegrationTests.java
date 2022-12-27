e0e5501b17dbf41ac66b07a0269b8483c0907562
huseyinaydin505 <huseyin.aydin@migrosonline.com>
2022-12-27 18:37:11 +0300

    #58 Develop integration tests

---
+++ infrasave/infrasave-rest-app/src/test/java/com/infrasave/ContentIntegrationTests.java
@@ -0,0 +1,156 @@
+package com.infrasave;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.infrasave.bean.AddContentRequest;
+import com.infrasave.bean.LoginRequest;
+import com.infrasave.bean.ModifyContentRequest;
+import com.infrasave.entity.User;
+import com.infrasave.entity.UserRole;
+import com.infrasave.enums.VisibilityLevel;
+import com.infrasave.repository.user.UserRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author huseyinaydin
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@TestPropertySource(
+    locations = "classpath:application-integrationtest.properties")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class ContentIntegrationTests {
+
+  private static ObjectMapper MAPPER = JsonMapper.builder()
+                                                 .findAndAddModules()
+                                                 .build();
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Order(1)
+  @Test
+  public void saveUser() throws Exception {
+    User user = new User();
+    user.setUsername("mehmet600");
+    user.setName("Mehmet");
+    user.setSurname("Selman");
+    user.setEmail("infrasave2.app@gmail.com");
+    user.setPassword(passwordEncoder.encode("123456"));
+    user.setPhoneNumber("+905111111111");
+    LocalDateTime now = LocalDateTime.now();
+    user.setBirthDate(now);
+    user.setLastUpdatedAt(now);
+    user.setCreatedAt(now);
+
+    UserRole userRole = new UserRole();
+    userRole.setRole("ROLE_USER");
+    userRole.setUser(user);
+    userRole.setCreatedAt(now);
+    userRole.setLastUpdatedAt(now);
+    user.setRoles(List.of(userRole));
+    userRepository.save(user);
+  }
+
+  @Test
+  @Order(2)
+  public void shouldGetMyFeed() throws Exception {
+    MockHttpSession session = getMockHttpSession();
+    mockMvc.perform(get("/contents/my-feed")
+                        .contentType(MediaType.APPLICATION_JSON).session(session))
+           .andExpect(status().isOk())
+           .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+           .andExpect(jsonPath("successful", is(true)));
+  }
+
+  @Test
+  @Order(3)
+  public void shouldCreateContent() throws Exception {
+    MockHttpSession session = getMockHttpSession();
+    AddContentRequest req =
+        new AddContentRequest(VisibilityLevel.EVERYONE, "Test-title", "test-url", "test-imageUrl", "Test-description",
+                              null);
+    String content = MAPPER.writeValueAsString(req);
+    mockMvc.perform(post("/contents")
+                        .contentType(MediaType.APPLICATION_JSON).session(session)
+                        .content(content))
+           .andExpect(status().isOk())
+           .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+           .andExpect(jsonPath("successful", is(true)));
+  }
+
+  @Test
+  @Order(4)
+  public void shouldUpdateContent() throws Exception {
+    MockHttpSession session = getMockHttpSession();
+    ModifyContentRequest req =
+        new ModifyContentRequest(1L, VisibilityLevel.EVERYONE, "Test-tit", "test-url", "test-imageUrl",
+                                 "Test-description",
+                                 null);
+    String content = MAPPER.writeValueAsString(req);
+    mockMvc.perform(put("/contents")
+                        .contentType(MediaType.APPLICATION_JSON).session(session)
+                        .content(content))
+           .andExpect(status().isOk())
+           .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+           .andExpect(jsonPath("successful", is(true)));
+  }
+
+  @Test
+  @Order(5)
+  public void shouldDeleteContent() throws Exception {
+    MockHttpSession session = getMockHttpSession();
+    mockMvc.perform(delete("/contents/" + "1")
+                        .contentType(MediaType.APPLICATION_JSON).session(session))
+           .andExpect(status().isOk())
+           .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+           .andExpect(jsonPath("successful", is(true)));
+  }
+
+  private MockHttpSession getMockHttpSession() throws Exception {
+    LoginRequest request = new LoginRequest("infrasave2.app@gmail.com", "123456");
+    String content = MAPPER.writeValueAsString(request);
+    ResultActions resultActions = mockMvc.perform(
+                                             post("/login").content(content)
+                                                           .contentType(MediaType.APPLICATION_JSON))
+                                         .andExpect(status().isOk())
+                                         .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                                         .andExpect(jsonPath("successful", is(true)));
+    MvcResult mvcResult = resultActions.andReturn();
+    MockHttpServletRequest mvcResultRequest = mvcResult.getRequest();
+    return (MockHttpSession) mvcResultRequest.getSession(false);
+  }
+}
